657a1a9f8216a2047a8f4d897f3f3bc5d6f3718a
Nastya <nastya.uchaneva@gmail.com>
2019-12-02 22:35:31 +0300

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,13 @@
+jQuery(document).ready(function() {
+    // добавление белого бэкграунда меню
+//    let nav = $('.navbar');
+//    $(window).scroll(function() {
+//        if ($(window).scrollTop()>100) {
+//            nav.addClass('nav-white');
+//            nav.addClass('fixed-top');            
+//        } else {
+//            nav.removeClass('nav-white');
+//            nav.removeClass('fixed-top');
+//        }
+//    })
+})
