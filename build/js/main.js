7bf3ad2f306aaa0b9671d0951bc78151e75a802b
Nastyauch <nastya.uchaneva@gmail.com>
2019-12-07 20:24:02 +0300

    fixed nav

--- build/js/main.js
+++ build/js/main.js
@@ -7,14 +7,14 @@
 //# sourceMappingURL=bootstrap.min.js.map
 jQuery(document).ready(function() {
     // добавление белого бэкграунда меню
-//    let nav = $('.navbar');
-//    $(window).scroll(function() {
-//        if ($(window).scrollTop()>100) {
-//            nav.addClass('nav-white');
-//            nav.addClass('fixed-top');            
-//        } else {
-//            nav.removeClass('nav-white');
-//            nav.removeClass('fixed-top');
-//        }
-//    })
+    let nav = $('.navbar');
+    $(window).scroll(function() {
+        if ($(window).scrollTop()>100) {
+            nav.addClass('nav-white');
+            nav.addClass('fixed-top');            
+        } else {
+            nav.removeClass('nav-white');
+            nav.removeClass('fixed-top');
+        }
+    })
 })
